e1d8ffa8d19d327d8bdf752257f6f7ff50d53c18
hugur@hotmail.com <gantep27>
2016-02-09 21:17:35 +0000

    sss

--- StockPrice/App/StockPrice/app.js
+++ StockPrice/App/StockPrice/app.js
@@ -6,7 +6,6 @@
     app.config(['$routeProvider', function ($routeProvider) {
 
         $routeProvider.when('/stockPrice', {
-
             templateUrl: 'app/stockprice/templates/stockPrice.html',
             controller: 'stockPriceController',
             controllerAs: 'vm',
